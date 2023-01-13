a5ccfe91c020b46c85034c5a27b23b8988e1f7ed
Gareth Flowers <garethflowers@users.noreply.github.com>
2023-01-13 20:50:04 +0000

    wip

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,6 @@ set -xe
 export IMAGE_NAME=garethflowers/npm-mirror
 
 docker build --tag $IMAGE_NAME .
-docker run --rm $IMAGE_NAME verdaccio -v
+docker run --rm $IMAGE_NAME verdaccio --version
 
 echo "\nOK"
