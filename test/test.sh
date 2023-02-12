4d3fe91df86d8cf9d8730187ffc7bab0850b3881
Gareth Flowers <garethflowers@users.noreply.github.com>
2023-02-12 16:22:44 +0000

    feat(): allow running as standard user

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,6 @@ set -xe
 export IMAGE_NAME=garethflowers/npm-mirror
 
 docker build --tag $IMAGE_NAME .
-docker run --rm $IMAGE_NAME verdaccio -v
+docker run --publish 4873:4873 --rm $IMAGE_NAME verdaccio
 
 echo "\nOK"
