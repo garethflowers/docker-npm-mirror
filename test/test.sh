5fd8a5d4dc91980827f982a11416a186f9c30f79
Gareth Flowers <garethflowers@users.noreply.github.com>
2023-04-18 10:03:48 +0100

    chore(): adds additional builds

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 set -xe
 export IMAGE_NAME=garethflowers/npm-mirror
 
-docker build --tag $IMAGE_NAME .
+docker build --no-cache --progress plain --tag $IMAGE_NAME .
 docker run --rm $IMAGE_NAME verdaccio --version
 
 echo "\nOK"
